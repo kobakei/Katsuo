50ff71f75c9670950a950bac19341de081c78c82
Keisuke Kobayashi <kobakei122@gmail.com>
2019-02-10 20:35:20 +0900

    Add entity module

--- app/src/main/java/io/github/kobakei/katsuo/timeline/MainActivity.kt
+++ app/src/main/java/io/github/kobakei/katsuo/timeline/MainActivity.kt
@@ -1,8 +1,8 @@
 package io.github.kobakei.katsuo.timeline
 
-import androidx.databinding.DataBindingUtil
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import androidx.lifecycle.get
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -25,9 +25,9 @@ class MainActivity : AppCompatActivity() {
         binding.viewModel = viewModel
 
         binding.recyclerView.adapter = MainAdapter(this, listOf(
-                Photo("title 1", "url 1"),
-                Photo("title 2", "url 2"),
-                Photo("title 3", "url 3")
+            Photo("title 1", "url 1"),
+            Photo("title 2", "url 2"),
+            Photo("title 3", "url 3")
         ))
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
     }
