993acc02bc9a20e11329fc2408046fee74b34de1
Keisuke Kobayashi <kobakei122@gmail.com>
2019-02-11 03:24:20 +0900

    Add author module

---
+++ ui/author/src/main/java/io/github/kobakei/katsuo/author/AuthorActivity.kt
@@ -0,0 +1,12 @@
+package io.github.kobakei.katsuo.author
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.author_activity)
+    }
+}
