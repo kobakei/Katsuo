afe5b44e75a2259b1a480c3355515d3a4c02c555
Keisuke Kobayashi <kobakei122@gmail.com>
2019-02-11 20:41:37 +0900

    Convert test to kotlin

---
+++ ui/detail/src/androidTest/java/io/github/kobakei/katsuo/detail/ExampleInstrumentedTest.kt
@@ -0,0 +1,25 @@
+package io.github.kobakei.katsuo.detail
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+
+        assertEquals("io.github.kobakei.katsuo.detail.test", appContext.getPackageName())
+    }
+}
