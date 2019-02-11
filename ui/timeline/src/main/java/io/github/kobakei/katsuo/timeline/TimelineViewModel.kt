b1c8491ba14c08cb075786158d7cba48ebb6cf63
Keisuke Kobayashi <kobakei122@gmail.com>
2019-02-11 23:37:50 +0900

    Show progress in timeline

--- ui/timeline/src/main/java/io/github/kobakei/katsuo/timeline/TimelineViewModel.kt
+++ ui/timeline/src/main/java/io/github/kobakei/katsuo/timeline/TimelineViewModel.kt
@@ -1,12 +1,12 @@
 package io.github.kobakei.katsuo.timeline
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import android.view.View
+import androidx.lifecycle.*
 import io.github.kobakei.katsuo.entity.Ad
 import io.github.kobakei.katsuo.entity.Article
 import io.github.kobakei.katsuo.repository.AdRepository
 import io.github.kobakei.katsuo.repository.ArticleRepository
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
@@ -18,14 +18,28 @@ class TimelineViewModel(
     val articles = MutableLiveData<List<Article>>()
     val ad = MutableLiveData<Ad>()
 
+    val progressVisibility: LiveData<Int> = Transformations.map(articles) {
+        if (it?.isNotEmpty() == true) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
     val articleClick = MutableLiveData<Article>()
 
     fun loadData() {
         viewModelScope.launch {
-            articles.postValue(articleRepo.getArticles())
-        }
-        viewModelScope.launch {
-            ad.postValue(adRepo.getTimelineAd())
+            val task1 = async {
+                articleRepo.getArticles()
+            }
+            val task2 = async {
+                adRepo.getTimelineAd()
+            }
+            val result1 = task1.await()
+            val result2 = task2.await()
+            articles.postValue(result1)
+            ad.postValue(result2)
         }
     }
 
