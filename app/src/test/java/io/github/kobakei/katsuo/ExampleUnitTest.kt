6de07e271eaf11f40de9443535d7912d5cc3fca8
Keisuke Kobayashi <kobakei122@gmail.com>
2019-04-20 00:15:33 +0900

    Fix test

--- app/src/test/java/io/github/kobakei/katsuo/ExampleUnitTest.kt
+++ app/src/test/java/io/github/kobakei/katsuo/ExampleUnitTest.kt
@@ -1,14 +1,16 @@
 package io.github.kobakei.katsuo
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.runner.RunWith
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@RunWith(AndroidJUnit4::class)
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
